1cc76f4f2467e062a07561f0d18bd44802db7de0
Charlie Jackson <charlie@charliejackson.com>
2017-04-22 16:01:15 +0100

    Dev task: Compile web pack first if it has not ran yet

--- web/scripts/watch.js
+++ web/scripts/watch.js
@@ -1,6 +1,8 @@
 import webpack from 'webpack';
 import { create } from 'browser-sync';
 import nodemon from 'nodemon';
+import fs from 'fs';
+import path from 'path';
 import options from '../webpack/index';
 import port from '../src/constants/port';
 import isProduction from './isProduction';
@@ -9,53 +11,64 @@ import listenLog from '../src/constants/listenLog';
 
 const bs = create();
 
-nodemon({
-  script: 'src/server/index.js',
-  exec: 'node node_modules/babel-cli/bin/babel-node.js',
-  ext: 'js, ejs, jsx',
-  ignore: ['scripts/**/*', 'webpack/**/*', 'src/public/*.js'],
-  stdout: false,
-});
-
 const production = isProduction();
 const compiler = webpack(options({ production }));
 
-let nodemonStarted = false;
-let bsStarted = false;
+function watch() {
+  let nodemonStarted = false;
+  let bsStarted = false;
 
-nodemon.on('quit', () => {
-  if (bsStarted) {
-    bs.exit();
-  }
-}).on('readable', function readable() {
-  this.stdout.setEncoding('utf8');
-  this.stderr.setEncoding('utf8');
-
-  this.stdout.on('data', (log) => {
-    // eslint-disable-next-line
-    console.log(log);
-
-    if (listenLog === log || log.includes(listenLog)) {
-      if (bsStarted) {
-        bs.reload();
-      }
-
-      if (!nodemonStarted) {
-        bs.init({
-          proxy: `localhost:${port}`,
-        }, () => {
-          bsStarted = true;
-
-          compiler.watch({}, (err, stats) => {
-            compileWebpack(err, stats, bs);
-          });
-        });
-
-        nodemonStarted = true;
-      }
-    }
+  nodemon({
+    script: 'src/server/index.js',
+    exec: 'node node_modules/babel-cli/bin/babel-node.js',
+    ext: 'js, ejs, jsx',
+    ignore: ['scripts/**/*', 'webpack/**/*', 'src/public/*.js'],
+    stdout: false,
   });
 
-  // eslint-disable-next-line
-  this.stderr.on('data', log => console.log(log));
-});
+  nodemon.on('quit', () => {
+    if (bsStarted) {
+      bs.exit();
+    }
+  }).on('readable', function readable() {
+    this.stdout.setEncoding('utf8');
+    this.stderr.setEncoding('utf8');
+
+    this.stdout.on('data', (log) => {
+      // eslint-disable-next-line
+      console.log(log);
+
+      if (listenLog === log || log.includes(listenLog)) {
+        if (bsStarted) {
+          bs.reload();
+        }
+
+        if (!nodemonStarted) {
+          bs.init({
+            proxy: `localhost:${port}`,
+          }, () => {
+            bsStarted = true;
+
+            compiler.watch({}, (err, stats) => {
+              compileWebpack(err, stats, bs);
+            });
+          });
+
+          nodemonStarted = true;
+        }
+      }
+    });
+
+    // eslint-disable-next-line
+    this.stderr.on('data', log => console.log(log));
+  });
+}
+
+if (fs.existsSync(path.join(__dirname, '../src/public'))) {
+  watch();
+} else {
+  compiler.run((err, stats) => {
+    compileWebpack(err, stats, bs);
+    watch();
+  });
+}
