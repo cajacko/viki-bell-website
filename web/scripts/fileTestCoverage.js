e0097fb772b4f05d44fffb1a853643d74b76df12
Charlie Jackson <charlie@charliejackson.com>
2017-05-01 10:11:36 +0100

    Started adding file test coverage script

---
+++ web/scripts/fileTestCoverage.js
@@ -0,0 +1,17 @@
+import glob from 'glob-fs';
+
+// // options is optional
+// glob('(**/*.js)', [], (er, files) => {
+//   // files is an array of filenames.
+//   // If the `nonull` option is set, and nothing
+//   // was found, then files is ["**/*.js"]
+//   // er is an error object or null.
+//   console.log(files);
+// });
+
+
+glob({ dotfiles: false })
+  // .exclude(/__generated__/)
+  .readdir('**/*.js', (err, files) => {
+    console.log(files);
+  });
