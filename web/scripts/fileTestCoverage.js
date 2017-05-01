f8f56fc4e6032546fff525d63c943214f7981e70
Charlie Jackson <contact@charliejackson.com>
2017-05-01 13:44:59 +0100

    Added file coverage checker

--- web/scripts/fileTestCoverage.js
+++ web/scripts/fileTestCoverage.js
@@ -1,18 +1,79 @@
 import glob from 'glob-fs';
+import fs from 'fs';
 
-// // options is optional
-// glob('(**/*.js)', [], (er, files) => {
-//   // files is an array of filenames.
-//   // If the `nonull` option is set, and nothing
-//   // was found, then files is ["**/*.js"]
-//   // er is an error object or null.
-//   console.log(files);
-// });
+const excludes = {
+  name: [
+    'test',
+  ],
+  path: [
+    '__generated__',
+    'webpack',
+  ],
+};
 
+function excludeFromArray(array, fileProp) {
+  // eslint-disable-next-line
+  for (let i = 0; i < array.length; i++) {
+    if (fileProp.includes(array[i])) {
+      return true;
+    }
+  }
 
-glob({ dotfiles: false })
-  // .exclude(/__generated__/)
+  return false;
+}
 
-  .readdir('**/*.js', (err, files) => {
-    console.log(files);
+function exclude(file) {
+  if (excludeFromArray(excludes.name, file.name)) {
+    return true;
+  }
+
+  if (excludeFromArray(excludes.path, file.relative)) {
+    return true;
+  }
+
+  return false;
+}
+
+const globPattern = glob({ dotfiles: false, gitignore: true })
+  .use((file) => {
+    if (exclude(file)) {
+      // eslint-disable-next-line
+      file.exclude = true;
+    }
+
+    return file;
   });
+
+globPattern.readdir('**/*.js', (err, files) => {
+  const fileCount = files.length;
+  const noTestFiles = [];
+
+  files.forEach((file) => {
+    const testFile = file.replace('.js', '.test.js');
+
+    if (!fs.existsSync(testFile)) {
+      noTestFiles.push(file);
+    }
+  });
+
+  const testedCount = fileCount - noTestFiles.length;
+
+  if (testedCount !== fileCount) {
+    // eslint-disable-next-line
+    console.log('Code coverage is not 100%!');
+    // eslint-disable-next-line
+    console.log('Write tests for the following files:');
+    // eslint-disable-next-line
+    noTestFiles.forEach(file => console.log(`  ${file}`));
+
+    const percentage = Math.round((testedCount / fileCount) * 100);
+
+    // eslint-disable-next-line
+    console.log(`Ratio: ${testedCount}/${fileCount} - ${percentage}%`);
+
+    throw new Error('Code coverage is not 100%!');
+  }
+
+  // eslint-disable-next-line
+  console.log(`Ratio: ${testedCount}/${fileCount}`);
+});
