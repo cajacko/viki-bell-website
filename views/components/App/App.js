39725ccca1ccdf6d04c24ebda4b07790d22ec5ed
Charlie Jackson <charlie@charliejackson.com>
2017-06-01 18:45:04 +0100

    Remove loading screen when App component mounts

--- views/components/App/App.js
+++ views/components/App/App.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import PostLoopContainer from 'containers/PostLoop/PostLoop';
 
-const App = () => <PostLoopContainer />;
+class App extends React.Component {
+  componentDidMount() {
+    window.initialRender = true;
+    const now = Math.floor(Date.now());
+    const loading = document.getElementById('loading');
+
+    if (window.noLoading) {
+      loading.remove();
+    } else {
+      const loadingTime = now - window.startTime;
+
+      // Otherwise looks like flash of content
+      const minimumLoadTime = 1000;
+      let additionalLoadTime = minimumLoadTime - loadingTime;
+
+      if (additionalLoadTime < 0) {
+        additionalLoadTime = 0;
+      }
+
+      setTimeout(() => {
+        loading.classList.remove('loading');
+
+        loading.addEventListener('transitionend', () => {
+          loading.remove();
+        }, true);
+      }, additionalLoadTime);
+    }
+  }
+
+  render() {
+    return <PostLoopContainer />;
+  }
+}
 
 export default App;
