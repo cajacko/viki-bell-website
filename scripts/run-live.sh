dd2ba98fcb5b28141ff3567c6d5a1b4b14d210f1
Charlie Jackson <contact@charliejackson.com>
2017-07-17 09:45:15 +0100

    Allow www subdomain

--- scripts/run-live.sh
+++ scripts/run-live.sh
@@ -1,5 +1,5 @@
 
 docker run -d --name viki-bell-website-mysql -v viki-bell-website:/var/lib/mysql --env-file .env mysql:5.7
 docker run -v viki-bell-website-wordpress:/var/www/html -e VIRTUAL_HOST=admin.vikibell.com --link viki-bell-website-mysql:viki-bell-website-mysql --expose 80 -d --env-file .env --name viki-bell-website-wordpress viki-bell-website-wordpress
-docker run -e VIRTUAL_HOST=vikibell.com --link viki-bell-website-mysql:viki-bell-website-mysql --expose 80 -d --env-file .env --name viki-bell-website-web viki-bell-website-web
+docker run -e VIRTUAL_HOST=vikibell.com,www.vikibell.com --link viki-bell-website-mysql:viki-bell-website-mysql --expose 80 -d --env-file .env --name viki-bell-website-web viki-bell-website-web
 docker exec viki-bell-website-web sh -c 'exec composer install --no-interaction'
