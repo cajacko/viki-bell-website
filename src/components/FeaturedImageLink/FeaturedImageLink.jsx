68359753b5d5d2c24a0b21d7bbd3ea1597e3ff9b
Charlie Jackson <contact@charliejackson.com>
2017-09-02 15:03:43 +0100

    Connected postLoopItem to contentful

---
+++ src/components/FeaturedImageLink/FeaturedImageLink.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Image from 'components/Image/Image';
+
+const FeaturedImageLink = (props) => {
+  const { url, file } = props;
+  const sizes = file.details.image;
+
+  return (
+    <Link
+      className="Post-headerLink"
+      to={url}
+      itemProp="image"
+      itemScope=""
+      itemType="https://schema.org/ImageObject"
+    >
+      <Image className="Post-featuredImage" {...props} />
+      <meta
+        className="Post-featuredImageUrl"
+        itemProp="url"
+        content={file.url}
+      />
+      <meta
+        className="Post-featuredImageWidth"
+        itemProp="width"
+        content={sizes.width}
+      />
+      <meta
+        className="Post-featuredImageHeight"
+        itemProp="height"
+        content={sizes.height}
+      />
+    </Link>
+  );
+};
+
+export default FeaturedImageLink;
--- src/components/PostLoopItem/PostLoopItem.jsx
+++ src/components/PostLoopItem/PostLoopItem.jsx
@@ -1,35 +1,69 @@
 import React from 'react';
+import Moment from 'moment';
 import SharePost from 'components/SharePost/SharePost';
+import Link from 'components/Link/Link';
+import postUrlFromSlug from 'helpers/postUrlFromSlug';
+import Item from 'components/Item/Item';
+import FeaturedImageLink from 'components/FeaturedImageLink/FeaturedImageLink';
 
-const PostLoopItem = ({ share }) => (
-  <article className="Post u-clearFix" itemProp="blogPost" itemScope="" itemType="http://schema.org/BlogPosting" itemRef="MicroData-person MicroData-organization">
-    <header>
-      <time className="Post-date" dateTime="2017-08-06 16:45:54" itemProp="datePublished">06.08</time>
-      <a className="Post-headerLink" href="/posts/my-july-highlights" itemProp="image" itemScope="" itemType="https://schema.org/ImageObject">
-        <img
-          className="Post-featuredImage"
-          alt="Mango-Tree"
-          src="https://admin.vikibell.com/wp-content/uploads/2017/08/Mango-Tree.jpg"
-          width="2000"
-          height="2000"
+const PostLoopItem = (props) => {
+  const {
+    share,
+    title,
+    wordpressContent,
+    postDate,
+    postSlug,
+    updatedAt,
+    featuredImage,
+  } = props;
+  const date = new Moment(postDate);
+  const url = postUrlFromSlug(postSlug);
+
+  return (
+    <article
+      className="Post u-clearFix"
+      itemProp="blogPost"
+      itemScope=""
+      itemType="http://schema.org/BlogPosting"
+      itemRef="MicroData-person MicroData-organization"
+    >
+      <header>
+        <time
+          className="Post-date"
+          dateTime={postDate}
+          itemProp="datePublished"
+        >
+          {date.format('DD.MM')}
+        </time>
+        <Item element={FeaturedImageLink} itemId={featuredImage} url={url} />
+        <Link className="Post-headerLink" to={url} itemProp="mainEntityOfPage">
+          <h2 className="Post-title" itemProp="headline">
+            {title}
+          </h2>
+        </Link>
+      </header>
+
+      <div
+        className="Post-content"
+        itemProp="articleBody"
+        dangerouslySetInnerHTML={{ __html: wordpressContent }}
+      />
+
+      <footer>
+        <meta
+          className="Post-dateModified"
+          content={updatedAt}
+          itemProp="dateModified"
         />
-        <meta className="Post-featuredImageUrl" itemProp="url" content="https://admin.vikibell.com/wp-content/uploads/2017/08/Mango-Tree.jpg" />
-        <meta className="Post-featuredImageWidth" itemProp="width" content="2000" />
-        <meta className="Post-featuredImageHeight" itemProp="height" content="2000" />
-      </a>
-      <a className="Post-headerLink" href="/posts/my-july-highlights" itemProp="mainEntityOfPage">
-        <h2 className="Post-title" itemProp="headline">My July Highlights</h2>
-      </a>
-    </header>
-
-    <div className="Post-content" itemProp="articleBody">Content</div>
-
-    <footer>
-      <meta className="Post-dateModified" content="2016-01-01 12:12:12" itemProp="dateModified" />
-      <meta className="Post-description" itemProp="description" content="My July Highlights | via vikibell.com" />
-      {share && <SharePost />}
-    </footer>
-  </article>
-);
+        <meta
+          className="Post-description"
+          itemProp="description"
+          content={`${title} | via vikibell.com`}
+        />
+        {share && <SharePost />}
+      </footer>
+    </article>
+  );
+};
 
 export default PostLoopItem;
