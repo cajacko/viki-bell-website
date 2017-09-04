05040af9bc093c01af9eb77e38f8082551ef380b
Charlie Jackson <contact@charliejackson.com>
2017-09-04 12:46:52 +0100

    Display tweets with actual data

--- src/components/Twitter/Twitter.component.jsx
+++ src/components/Twitter/Twitter.component.jsx
@@ -2,12 +2,33 @@
 
 import React, { Component } from 'react';
 // import TwitterApi from 'twitter';
+import Tweet from 'components/Tweet/Tweet.component';
 import Link from 'components/Link/Link';
 import Icon from 'components/Icon/Icon';
 
+function spliceTweets(tweets) {
+  const arr = Object.assign([], tweets);
+
+  return arr.slice(0, 5);
+}
+
 class Twitter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { tweets: spliceTweets(this.props.tweets) };
+  }
+
   componentDidMount() {
     this.props.getTweets();
+
+    setInterval(() => {
+      this.props.getTweets();
+    }, 1000 * 60);
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({ tweets: spliceTweets(props.tweets) })
   }
 
   render() {
@@ -21,48 +42,7 @@ class Twitter extends Component {
         </header>
 
         <div>
-          <article className="Tweet">
-            <header className="u-clearFix">
-              <a className="Tweet-dateLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <time className="Tweet-date" dateTime="2017-08-07 09:21:50">4 hours ago</time>
-              </a>
-              <a className="Tweet-imageLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <img className="Tweet-image" src="http://pbs.twimg.com/profile_images/815141633360531456/_Ko6DRY-_normal.jpg" alt="Viki bell twitter" />
-              </a>
-              <a className="Tweet-nameLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <h3 className="Tweet-name">Viki Bell</h3>
-              </a>
-              <a className="Tweet-usernameLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <span className="Tweet-username">@Vikiibell</span>
-              </a>
-            </header>
-            <div className="Tweet-content">
-              So <a href="http://twitter.com/charliejackson" target="_blank" rel="noopener noreferrer">@charliejackson</a> set up a compliment bot to send me fun messages every Monday... this one though 😂😂😂😂😂 <a href="https://t.co/0NTNyiYaXS" target="_blank" rel="noopener noreferrer">https://t.co/0NTNyiYaXS</a>
-            </div>
-          </article>
-
-          <article className="Tweet">
-            <header className="u-clearFix">
-              <a className="Tweet-dateLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <time className="Tweet-date" dateTime="2017-08-06 21:06:50">16 hours ago</time>
-              </a>
-              <a className="Tweet-imageLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <img className="Tweet-image" src="http://pbs.twimg.com/profile_images/815141633360531456/_Ko6DRY-_normal.jpg" alt="Viki bell twitter" />
-              </a>
-              <a className="Tweet-nameLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <h3 className="Tweet-name">Viki Bell</h3>
-              </a>
-              <a className="Tweet-usernameLink" href="http://twitter.com/Vikiibell" target="_blank" rel="noopener noreferrer">
-                <span className="Tweet-username">@Vikiibell</span>
-              </a>
-            </header>
-            <div className="Tweet-content">
-              Currently sat in the garden on our new sofas, listening to Soul Sunday on Spotify and watching the fire 😍 <a href="https://t.co/nMmW9zR8qC" target="_blank" rel="noopener noreferrer">https://t.co/nMmW9zR8qC</a>
-            </div>
-            <a className="Tweet-contentImageLink" href="">
-              <img className="Tweet-contentImage" src="http://pbs.twimg.com/media/DGkmywdXcAAYTea.jpg" alt="Featured" />
-            </a>
-          </article>
+          {this.state.tweets.map(tweet => <Tweet key={tweet.id} {...tweet} />)}
         </div>
 
         <footer>
