1b74d862e019a9cd84d4a03a2be25795efdaf67a
Charlie Jackson <contact@charliejackson.com>
2017-09-11 19:39:14 +0100

    Migrate posts

--- scripts/migrateData.js
+++ scripts/migrateData.js
@@ -1,27 +1,196 @@
-/* eslint no-console: 0 */
-
-import { parseString } from 'xml2js';
-import { readFileSync, writeFile } from 'fs';
+import { createClient } from 'contentful-management';
+import { writeFile, readFile } from 'fs';
 import { join } from 'path';
-import parseWordpressXml from './migration/parseWordpressXml';
+import moment from 'moment';
+import { posts, images } from './items.json';
+import imageMapFile from './imageMapFile.json';
+import tagMapFile from './tagMapFile.json';
+import categoryMapFile from './categoryMapFile.json';
 
-const xml = readFileSync(join(__dirname, 'migration/data.xml'), 'utf8');
+const client = createClient({
+  accessToken: '',
+});
 
-parseString(xml, (err, result) => {
-  if (err) {
-    throw err;
+const mapFile = join(__dirname, './postMapFile.json');
+
+function getMapFile(asset) {
+  return new Promise((resolve, reject) => {
+    readFile(mapFile, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const json = JSON.parse(data);
+
+        if (asset) {
+          resolve({ data: json, asset });
+        } else {
+          resolve(json);
+        }
+      }
+    });
+  });
+}
+
+function saveContent(wpID, fields) {
+  // console.log(fields);
+  return client
+    .getSpace('3ctegf19trkf')
+    .then(space => space.createEntry('post', { fields }))
+    .then(asset => getMapFile(asset))
+    .then(
+      ({ asset, data }) =>
+        new Promise((resolve, reject) => {
+          let newFile = Object.assign({}, data);
+          newFile[wpID] = asset;
+          newFile = JSON.stringify(newFile, null, 2);
+
+          writeFile(mapFile, newFile, 'utf8', (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          });
+        }),
+    )
+    .catch(console.error);
+}
+
+function replaceImages(content) {
+  let newContent = content;
+
+  newContent = newContent.replace(/-[0-9]*x[0-9]*./g, '.');
+
+  Object.keys(images).forEach((imageId) => {
+    const { resource } = images[imageId];
+    let regex = resource;
+    regex = regex.replace(/\//g, '\\/');
+    regex = regex.replace(/\./g, '\\.');
+    let re = new RegExp(regex, 'g');
+
+    const replacement = imageMapFile[imageId].fields.file['en-GB'].url;
+
+    newContent = newContent.replace(re, replacement);
+    regex = regex.replace('https:', 'http:');
+    re = new RegExp(regex, 'g');
+    newContent = newContent.replace(re, replacement);
+  });
+
+  // console.log('\n\n\n');
+  // console.log(newContent);
+
+  return newContent;
+}
+
+function saveContentLoop(items, i) {
+  const item = items[i];
+
+  if (!item) {
+    return;
   }
 
-  const items = parseWordpressXml(result);
+  Object.keys(item.item).forEach((prop) => {
+    switch (prop) {
+      case 'title':
+      case 'postSlug':
+      case 'wordpressContent':
+      case 'contentType':
+      case 'id':
+      case 'postDate':
+      case 'status':
+      case 'categories':
+      case 'tags':
+      case 'seoDescription':
+      case 'seoTitle':
+      case 'featuredImage':
+        break;
+      default:
+        console.log(prop);
+        break;
+    }
+  });
 
-  console.log(items);
+  const fields = {
+    title: {
+      'en-GB': item.item.title,
+    },
+    postSlug: {
+      'en-GB': item.item.postSlug,
+    },
+    wordpressContent: {
+      'en-GB': replaceImages(item.item.wordpressContent),
+    },
+    postDate: {
+      'en-GB': moment(item.item.postDate).toISOString(),
+    },
+    tags: { 'en-GB': [] },
+    categories: { 'en-GB': [] },
+  };
 
-  writeFile('items.json', JSON.stringify(items, null, 2), 'utf8', (err) => {});
+  if (item.item.seoTitle) {
+    fields.seoTitle = {
+      'en-GB': item.item.seoTitle,
+    };
+  }
 
-  // save content with no references - categories, images, tags
-  // Make sure we have json reference files for these
-  // Save posts and pages, whilst replacing linked content with contentful resources
-  // For each post and page, get the html from the website and save in new field
-  // Replace the images in these fields with contentful images
-  // All good :)
+  if (item.item.seoDescription) {
+    fields.seoDescription = {
+      'en-GB': item.item.seoDescription,
+    };
+  }
+
+  if (item.item.featuredImage) {
+    fields.featuredImage = {
+      'en-GB': {
+        sys: {
+          type: 'Link',
+          linkType: 'Asset',
+          id: imageMapFile[item.item.featuredImage].sys.id,
+        },
+      },
+    };
+  }
+
+  item.item.tags.forEach((tag) => {
+    fields.tags['en-GB'].push({
+      sys: {
+        type: 'Link',
+        linkType: 'Entry',
+        id: tagMapFile[tag.slug].sys.id,
+      },
+    });
+  });
+
+  item.item.categories.forEach((category) => {
+    fields.categories['en-GB'].push({
+      sys: {
+        type: 'Link',
+        linkType: 'Entry',
+        id: categoryMapFile[category.slug].sys.id,
+      },
+    });
+  });
+
+  saveContent(item.wpID, fields)
+    .then(() => {
+      saveContentLoop(items, i + 1);
+    })
+    .catch(console.error);
+
+  // saveContentLoop(items, i + 1);
+}
+
+const loop = [];
+let i = 0;
+
+Object.keys(posts).forEach((wpID) => {
+  if (i > 5) {
+    // return;
+  }
+
+  const item = posts[wpID];
+  loop.push({ item, wpID });
+  i += 1;
 });
+
+saveContentLoop(loop, 0);
