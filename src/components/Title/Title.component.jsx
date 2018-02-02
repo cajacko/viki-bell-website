f7fa29c9e5ebab328f057badeddcff8e16b2be8c
Charlie Jackson <charlie.jackson@eventstag.com>
2018-02-02 14:58:23 +0000

    Show the title on the post page, with correct data

--- src/components/Title/Title.component.jsx
+++ src/components/Title/Title.component.jsx
@@ -1,21 +1,38 @@
 import React, { PureComponent } from 'react';
+import moment from 'moment';
 import TitleRender from 'components/Title/Title.render';
 import Item from 'components/Item/Item';
 
 class Title extends PureComponent {
   render() {
     let title;
+    let dateString;
+    let dateDisplay;
 
     switch (this.props.contentType) {
       case 'category':
         title = `Category: ${this.props.title}`;
         break;
 
+      case 'post': {
+        title = this.props.title;
+        dateString = this.props.postDate;
+        dateDisplay = 'March 24, 2017';
+        dateDisplay = moment(dateString).format('MMMM Do, YYYY');
+        break;
+      }
+
       default:
         break;
     }
 
-    return <TitleRender title={title} dateDisplay={null} dateString={null} />;
+    return (
+      <TitleRender
+        title={title}
+        dateDisplay={dateDisplay}
+        dateString={dateString}
+      />
+    );
   }
 }
 
